b35025bb901808095711c75940a41644579f5549
Shaun Assam <thebeardedhermit@gmail.com>
2026-01-29 19:41:08 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,19 +8,12 @@ ZIP_NAME=$UUID.zip
 
 cd src/v-45-46-47-48-49
 
-echo -e "\t2. Creating zip file..."
+echo -e "Creating zip file..."
 zip -qr $ZIP_NAME ./* && echo -e "\t3. zip file created"
 
-echo -e "\t4. Installing the extension from the zip file...\n"
+echo -e "Installing the extension from the zip file...\n"
 
 gnome-extensions install -f $ZIP_NAME
 rm -rf $ZIP_NAME
 
-echo -e "\t------------------------------------------
-\t| Bring Out Submenu Of Power Off Button Extension is installed |
-\t------------------------------------------
-
-\tNow please enable the extension, once extension is enabled then
-\tYou can show/hide buttons as you wish from extensions settings"
-echo -e "\n\t~~~~~~~~~~~~~~~~~~ Thank You ~~~~~~~~~~~~~~~~~~\n"
 exit 0
