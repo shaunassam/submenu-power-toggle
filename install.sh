c3917f9446670307e2bb1278cc1c58c321ebdf34
Shaun Assam <thebeardedhermit@gmail.com>
2026-01-29 19:39:39 -0500

    Update install.sh; removed GNOME version check to build RPM

--- install.sh
+++ install.sh
@@ -6,22 +6,7 @@ DOMAIN=pratap.fastmail.fm
 UUID=$NAME@$DOMAIN
 ZIP_NAME=$UUID.zip
 
-# Findout gnome-shell version
-SHELL_VERSION=$(gnome-shell --version | cut -d ' ' -f3 | cut -d '.' -f1)
-
-if [[ $SHELL_VERSION -lt 45 ]]
-then
-    echo "This script is not for the gnome-shell versions below 45, Exiting with no changes."
-    exit 1
-fi
-
-echo -e "\n\n\t~~~~~~~~~~~~~~~~ Bring Out Submenu Of Power Off Button ~~~~~~~~~~~~~~~~\n"
-echo -e "\trunning the script...\n"
-echo -e "\t1. gnome-shell version $SHELL_VERSION detected"
-
-if [[ $SHELL_VERSION -ge 45 ]];then
-  cd src/v-45-46-47-48-49
-fi
+cd src/v-45-46-47-48-49
 
 echo -e "\t2. Creating zip file..."
 zip -qr $ZIP_NAME ./* && echo -e "\t3. zip file created"
